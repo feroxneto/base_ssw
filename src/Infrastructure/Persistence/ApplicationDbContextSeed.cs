61e11d56c4a2278dade0ec70b8ac46f9ae62e366
Ferox Neto <feroxneto@gmail.com>
2020-03-23 17:35:14 +0100

    Initial commit

---
+++ src/Infrastructure/Persistence/ApplicationDbContextSeed.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchitecture.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Persistence
+{
+    public static class ApplicationDbContextSeed
+    {
+        public static async Task SeedAsync(UserManager<ApplicationUser> userManager)
+        {
+            var defaultUser = new ApplicationUser { UserName = "administrator@localhost", Email = "administrator@localhost" };
+
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                await userManager.CreateAsync(defaultUser, "Administrator1!");
+            }
+        }
+    }
+}
